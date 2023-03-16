d8cfb141fb02485b1c1811a14dae61d2fdcc50ff
tomcatprime <karolchru@gmail.com>
2023-03-16 20:57:00 +0200

    second commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // first commit
+        // My Second stage
         [Key]
         public int CartItemId { get; set; }
 
